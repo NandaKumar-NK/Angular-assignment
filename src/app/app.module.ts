8b54f625354498aa8bed9d8a5b5f05aad745a97f
NandaKumar-NK <nanda.kanini@gmail.com>
2023-07-07 16:39:22 +0530

    work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { UserNameComponent } from './user-name/user-name.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    UserNameComponent,
+    CalculatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
